ffe0818db3792c5f08d529fbadf1eb8992dac2b3
neeraj kumar <neerajkumar08697@gmail.com>
2024-11-10 17:40:07 +0530

    config dotenv

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
   return res.json({ message: "server is ready now" });
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`server started at http://localhost:5000/`);
 });
