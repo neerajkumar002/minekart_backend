c02d00ee5a2d68d829d261f76c5ca06b3f8f6178
Neeraj Kumar <neerajkumar.mobail@gmail.com>
2024-11-11 11:20:00 +0530

    Added get all products route

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createProduct } from "../controller/product/product.controller.js";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controller/product/product.controller.js";
 
 const router = express.Router();
 
 // create product route
 router.post("/create", createProduct);
-
+// get all products route
+router.get("/", getAllProducts);
 export default router;
